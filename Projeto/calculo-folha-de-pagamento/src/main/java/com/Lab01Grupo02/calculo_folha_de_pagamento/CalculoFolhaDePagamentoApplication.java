f7b7a73b5b52d885c37d3e9abc89df2e74d1a84b
Miukiyn <71103276+Miukiyn@users.noreply.github.com>
2025-09-08 13:44:16 -0300

    Esqueleto Inicial do Projeto

---
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/CalculoFolhaDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.Lab01Grupo02.calculo_folha_de_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoFolhaDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoFolhaDePagamentoApplication.class, args);
+	}
+
+}
