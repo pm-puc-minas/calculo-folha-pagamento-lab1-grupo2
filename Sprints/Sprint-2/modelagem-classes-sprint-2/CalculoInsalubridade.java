5dfcedcf3ad541db1bfd21eda796403a999e99d1
murilofsouzaa <onemurilo@gmail.com>
2025-10-05 15:08:08 -0300

    Criação da interface ICalculadora + Atualização da classe CalculoInsalubridade

--- Sprints/Sprint-2/modelagem-classes-sprint-2/CalculoInsalubridade.java
+++ Sprints/Sprint-2/modelagem-classes-sprint-2/CalculoInsalubridade.java
@@ -1,9 +1,19 @@
-package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
-
 import java.math.BigDecimal;
 
-public class CalculoInsalubridade {
-    public BigDecimal calcularAdicional(double salarioMinimo, double grauRisco, String descricao) {
-        return BigDecimal.valueOf(salarioMinimo * grauRisco); // Exemplo
+public class CalculoInsalubridade implements ICalculadora {
+
+    @Override
+    public FolhaDePagamento calcularFolha(double salarioMinimo, double grauRisco, String descricao) {
+        FolhaDePagamento folha = new FolhaDePagamento();
+        
+        BigDecimal adicional = BigDecimal.valueOf(salarioMinimo * grauRisco);
+        
+        folha.setDescricao(descricao);
+        folha.setValor(adicional);
+        return folha;
+    }
+    
+    public BigDecimal calcularAdicional(double salarioMinimo, double grauRisco) {
+        return BigDecimal.valueOf(salarioMinimo * grauRisco);
     }
 }
---
+++ Sprints/Sprint-2/modelagem-classes-sprint-2/ICalculadora.java
@@ -0,0 +1,5 @@
+import FolhaDePagamento;
+
+public interface ICalculadora {
+	FolhaDePagamento calcularFolha();
+}
