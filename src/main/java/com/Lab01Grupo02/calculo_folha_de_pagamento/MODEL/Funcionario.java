e936841014d097683d73ecbdbc9c02211d2c7204
Bruno Hoske <brunohoske@gmail.com>
2025-10-09 18:43:29 -0300

    Melhorias para utilização no banco

--- src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Funcionario.java
+++ src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Funcionario.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,19 +15,34 @@ import lombok.Setter;
 @NoArgsConstructor // Define um construtor sem argumentos
 @AllArgsConstructor // Define um construtor com argumentos
 
-public class Funcionario extends Pessoa  {
+
+@Entity
+@Table(name = "FUNCIONARIOS")
+@PrimaryKeyJoinColumn(name = "ID_Pessoa")
+public class Funcionario extends Pessoa {
+
+    @Column(name = "SalarioBruto")
     private BigDecimal salarioBruto;
+
+    @Column(name = "DataAdmissao")
     private LocalDate dataAdmissao;
+
+    @Column(name = "TemPericulosidade")
     private boolean temPericulosidade;
+
+    @Column(name = "TemInsalubridade")
     private boolean temInsalubridade;
+
+    @Column(name = "GrauInsalubridade")
     private String grauInsalubridade;
+    @Column(name = "Cargo")
     private String cargo;
+
+    // Relação com Dependente
+    @OneToMany(mappedBy = "funcionario", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Dependente> dependentes;
-    
-    public int GetQuantidadeDependentes()
-    {
-        return dependentes.size();
+
+    public int getQuantidadeDependentes() {
+        return dependentes != null ? dependentes.size() : 0;
     }
-
-
 }
