e42b391649fa14629b86890d8031fd439d077bd7
Bruno Hoske <brunohoske@gmail.com>
2025-10-09 18:19:56 -0300

    Construtores Pessoa

--- src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Pessoa.java
+++ src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Pessoa.java
@@ -1,10 +1,15 @@
 package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter // Define os métodos getters para todos os campos
 @Setter // Define os métodos setters para todos os campos
+@NoArgsConstructor // Define um construtor sem argumentos
+@AllArgsConstructor // Define um construtor com argumentos
+
 
 public class Pessoa {
     private String nome;
