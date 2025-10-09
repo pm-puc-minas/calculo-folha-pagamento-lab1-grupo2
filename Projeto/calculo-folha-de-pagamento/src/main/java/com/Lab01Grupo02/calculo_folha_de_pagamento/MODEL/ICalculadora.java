804034cae1b60f689f0ffa6de1ece80ebe31cbd4
murilofsouzaa <onemurilo@gmail.com>
2025-10-08 23:20:20 -0300

    Remoção da ICalculadora de MODEL

--- Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/ICalculadora.java
+++
@@ -1,6 +0,0 @@
-package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
-import java.math.BigDecimal;
-
-public interface ICalculadora {
-    FolhaDePagamento calcularFolha(double salarioMinimo, double grauRisco, String descricao);
-}
