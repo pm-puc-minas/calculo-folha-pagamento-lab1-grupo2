ac68486cb6967b99025838ceae4180d121e7b543
Bruno Hoske <brunohoske@gmail.com>
2025-10-09 18:19:38 -0300

    construtores Dependente

--- src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Dependente.java
+++ src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Dependente.java
@@ -3,9 +3,16 @@ package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 import lombok.Getter;
 import lombok.Setter;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter // Define os métodos getters para todos os campos
 @Setter // Define os métodos setters para todos os campos
-    
+@NoArgsConstructor // Define um construtor sem argumentos
+@AllArgsConstructor // Define um construtor com argumentos
+
 public class Dependente extends Pessoa {
     private String parentesco;
 }
