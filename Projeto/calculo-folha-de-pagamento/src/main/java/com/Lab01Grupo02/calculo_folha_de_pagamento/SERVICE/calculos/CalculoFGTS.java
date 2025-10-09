900ef764662c2344565995b73cb7c3dcfec84de7
murilofsouzaa <onemurilo@gmail.com>
2025-10-08 23:34:20 -0300

    refatoração no calculo FGTS

--- Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/CalculoFGTS.java
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/CalculoFGTS.java
@@ -7,11 +7,10 @@ public class CalculoFGTS {
 
     private static final BigDecimal ALIQUOTA = new BigDecimal("0.08");
 
-    public BigDecimal calcularFGTS(BigDecimal salarioBruto) {
+    public BigDecimal calcular(BigDecimal salarioBruto) {
         if (salarioBruto == null) {
             return BigDecimal.ZERO;
         }
-        // Multiplica e arredonda para 2 casas decimais
         return salarioBruto.multiply(ALIQUOTA).setScale(2, RoundingMode.HALF_UP);
     }
 }
