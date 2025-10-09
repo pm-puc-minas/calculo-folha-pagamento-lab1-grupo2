777fb5a50313328215729c22f4594297f310bf17
murilofsouzaa <onemurilo@gmail.com>
2025-10-09 12:14:00 -0300

    Correção de erros no calculo insalubridade

--- Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/CalculoInsalubridade.java
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/CalculoInsalubridade.java
@@ -4,11 +4,37 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL.FolhaDePagamento;
 
-public class CalculoInsalubridade {
+public class CalculoInsalubridade implements ICalculadora {
 
-    public FolhaDePagamento calcularFolha(double salarioBruto, double percentual, String grau) {
-        BigDecimal salario = BigDecimal.valueOf(salarioBruto);
-        BigDecimal totalProvento = salario.multiply(BigDecimal.valueOf(percentual))
+    private String grau; // Baixo, Médio, Alto
+
+    public CalculoInsalubridade(String grau) {
+        this.grau = grau;
+    }
+
+    @Override
+    public FolhaDePagamento calcularFolha(BigDecimal salarioBruto) {
+        if (salarioBruto == null) {
+            salarioBruto = BigDecimal.ZERO;
+        }
+
+        double percentual = 0.0;
+        switch (grau.toLowerCase()) {
+            case "baixo":
+                percentual = 0.10;
+                break;
+            case "médio":
+            case "medio":
+                percentual = 0.20;
+                break;
+            case "alto":
+                percentual = 0.40;
+                break;
+            default:
+                percentual = 0.0; // caso inválido
+        }
+
+        BigDecimal totalProvento = salarioBruto.multiply(BigDecimal.valueOf(percentual))
                 .setScale(2, RoundingMode.HALF_UP);
 
         FolhaDePagamento folha = new FolhaDePagamento();
