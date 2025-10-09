e502f1aac3a31867d70cc88d97d6c6a0c101b14d
murilofsouzaa <onemurilo@gmail.com>
2025-10-09 12:12:26 -0300

    Correção de erros no calculo insalubridade

--- Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/CalculoInsalubridade.java
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/CalculoInsalubridade.java
@@ -3,22 +3,16 @@ package com.Lab01Grupo02.calculo_folha_de_pagamento.SERVICE.calculos;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL.FolhaDePagamento;
-import com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL.ICalculadora;
 
-public class CalculoInsalubridade implements ICalculadora {
+public class CalculoInsalubridade {
 
-    @Override
-    public FolhaDePagamento calcularFolha(double salarioMinimo, double grauRisco, String descricao) {
-        BigDecimal salarioMinimoBD = BigDecimal.valueOf(salarioMinimo);
-        BigDecimal grauRiscoBD = BigDecimal.valueOf(grauRisco);
-
-        BigDecimal adicional = salarioMinimoBD.multiply(grauRiscoBD).setScale(2, RoundingMode.HALF_UP);
+    public FolhaDePagamento calcularFolha(double salarioBruto, double percentual, String grau) {
+        BigDecimal salario = BigDecimal.valueOf(salarioBruto);
+        BigDecimal totalProvento = salario.multiply(BigDecimal.valueOf(percentual))
+                .setScale(2, RoundingMode.HALF_UP);
 
         FolhaDePagamento folha = new FolhaDePagamento();
-        folha.setDescricao(descricao);
-        folha.setTotalProvento(adicional);
-        folha.setSalarioLiquido(adicional); // se não há descontos
-
+        folha.setTotalProvento(totalProvento);
         return folha;
     }
 }
