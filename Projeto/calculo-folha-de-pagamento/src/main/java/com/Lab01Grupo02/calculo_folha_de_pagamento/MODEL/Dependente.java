78ecf09eb54b01b61b48b192da40a29888c315ce
Miukiyn <iagoadrien@gmail.com>
2025-10-09 18:59:36 -0300

    Manutenção na estrutura da aplicação

--- Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Dependente.java
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Dependente.java
@@ -1,5 +1,27 @@
 package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Getter // Define os métodos getters para todos os campos
+@Setter // Define os métodos setters para todos os campos
+@NoArgsConstructor // Define um construtor sem argumentos
+@AllArgsConstructor // Define um construtor com argumentos
+
+@Entity
+@Table(name = "DEPENDENTES")
+@PrimaryKeyJoinColumn(name = "ID_Pessoa")
 public class Dependente extends Pessoa {
+
+    @Column(name = "Parentesco")
     private String parentesco;
+    // Muitos dependentes para um funcionário
+    @ManyToOne
+    @JoinColumn(name = "ID_Funcionario")
+    private Funcionario funcionario;
 }
--- Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Pessoa.java
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Pessoa.java
@@ -1,7 +1,38 @@
 package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Getter // Define os métodos getters para todos os campos
+@Setter // Define os métodos setters para todos os campos
+@NoArgsConstructor // Define um construtor sem argumentos
+@AllArgsConstructor // Define um construtor com argumentos
+
+@Entity
+@Table(name = "PESSOAS")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_Pessoa")
+    private int IdPessoa;
+    @Column(name = "Nome")
     private String nome;
+    @Column(name = "Cpf")
     private String cpf;
-    private String dataNascimento;
+    @Column(name = "DataNascimento")
+    private LocalDate dataNascimento;
+
+    public int getIdade() {
+        if (dataNascimento == null) {
+            return 0;
+        }
+        return Period.between(dataNascimento, LocalDate.now()).getYears();
+    }
+
 }
