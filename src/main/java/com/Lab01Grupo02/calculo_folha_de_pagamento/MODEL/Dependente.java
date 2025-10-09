daab0b4dec21feed8e5fc1480f7d9ee23d4d822a
Bruno Hoske <brunohoske@gmail.com>
2025-10-09 18:08:30 -0300

    Getters e Setters classe Dependente.java

--- src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Dependente.java
+++ src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Dependente.java
@@ -1,5 +1,11 @@
 package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter // Define os métodos getters para todos os campos
+@Setter // Define os métodos setters para todos os campos
+    
 public class Dependente extends Pessoa {
     private String parentesco;
 }
