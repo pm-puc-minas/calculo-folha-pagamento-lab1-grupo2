7898e7016be95c0d07223bdd2ca730c8e4b306c5
Miukiyn <iagoadrien@gmail.com>
2025-10-09 19:00:14 -0300

    Manutenção na estrutura da aplicação

--- src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/SERVICE/calculos/ICalculadora.java
+++
@@ -1,8 +0,0 @@
-package com.Lab01Grupo02.calculo_folha_de_pagamento.SERVICE.calculos;
-
-import com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL.FolhaDePagamento;
-import java.math.BigDecimal;
-
-public interface ICalculadora {
-    FolhaDePagamento calcularFolha(BigDecimal salarioBruto);
-}
