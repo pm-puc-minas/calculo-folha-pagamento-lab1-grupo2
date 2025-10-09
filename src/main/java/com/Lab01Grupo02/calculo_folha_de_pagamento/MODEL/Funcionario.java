95e33603cd4c7d3b9ddad1cbbe7dec9842ad1766
Bruno Hoske <brunohoske@gmail.com>
2025-10-09 18:11:47 -0300

    Getters e Setters Funcionario adicionado

--- src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Funcionario.java
+++ src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/MODEL/Funcionario.java
@@ -4,6 +4,16 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter // Define os métodos getters para todos os campos
+@Setter // Define os métodos setters para todos os campos
+@NoArgsConstructor // Define um construtor sem argumentos
+@AllArgsConstructor // Define um construtor com argumentos
+
 public class Funcionario  {
     private BigDecimal salarioBruto;
     private LocalDate dataAdmissao;
