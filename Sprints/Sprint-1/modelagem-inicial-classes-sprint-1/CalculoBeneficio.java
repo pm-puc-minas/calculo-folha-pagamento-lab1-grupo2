0bfe1cc5cee0e9a44a0f0ebac15fc2482febacfb
Iagolara <iagodekara@gmail.com>
2025-11-08 19:37:22 -0300

    feat: implementacao de calculo de beneficios e ajustes na folha de pagamento

--- Sprints/Sprint-1/modelagem-inicial-classes-sprint-1/CalculoBeneficio.java
+++ Sprints/Sprint-1/modelagem-inicial-classes-sprint-1/CalculoBeneficio.java
@@ -1,13 +1,19 @@
 package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculoBeneficio {
-    public double calcularVT(double salarioBruto, double valorVT) {
-        return salarioBruto * 0.06; // Exemplo
+    
+
+    public BigDecimal calcularVT(BigDecimal salarioBruto, BigDecimal valorVTTotal) {
+        BigDecimal descontoLegal = salarioBruto.multiply(new BigDecimal("0.06")); 
+        
+  
+        return descontoLegal.min(valorVTTotal).setScale(2, RoundingMode.HALF_UP);
     }
 
-    public BigDecimal calcularVA(int diasUteis, double valorDiario) {
-        return BigDecimal.valueOf(diasUteis * valorDiario);
+    public BigDecimal calcularVA(int diasUteis, BigDecimal valorDiario) {
+        return valorDiario.multiply(BigDecimal.valueOf(diasUteis)).setScale(2, RoundingMode.HALF_UP);
     }
 }
--- Sprints/Sprint-1/modelagem-inicial-classes-sprint-1/CalculoFolhaPagamento.java
+++ Sprints/Sprint-1/modelagem-inicial-classes-sprint-1/CalculoFolhaPagamento.java
@@ -1,8 +1,8 @@
 package com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL;
 
-
 import com.Lab01Grupo02.calculo_folha_de_pagamento.MODEL.FolhaPagamento;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculoFolhaPagamento {
 
@@ -13,18 +13,42 @@ public class CalculoFolhaPagamento {
     private final CalculoBeneficio calculoBeneficios = new CalculoBeneficio();
     private final CalculoFGTS calculoFGTS = new CalculoFGTS();
 
-    public FolhaPagamento calcularFolha(double salarioBruto) {
+    public FolhaPagamento calcularFolha(double salarioBase) {
         FolhaPagamento folha = new FolhaPagamento();
 
-        folha.setSalarioBruto(BigDecimal.valueOf(salarioBruto));
-        folha.setBaseINSS(calculoINSS.calcularDesconto(salarioBruto));
-        folha.setBaseFGTS(calculoFGTS.calcularFGTS(salarioBruto));
+        BigDecimal salarioBruto = BigDecimal.valueOf(salarioBase).setScale(2, RoundingMode.HALF_UP);
+        folha.setSalarioBruto(salarioBruto);
 
-        folha.setSalarioLiquido(
-            folha.getSalarioBruto()
-                .subtract(folha.getBaseINSS())
-                .subtract(folha.getBaseFGTS())
-        );
+        BigDecimal adicionalPericulosidade = calculoPericulosidade.calcularAdicional(salarioBase);
+        BigDecimal adicionalInsalubridade = calculoInsalubridade.calcularAdicional(salarioBase);
+        BigDecimal totalBeneficios = calculoBeneficios.calcularTotalBeneficios(salarioBase);
+
+        BigDecimal salarioBrutoComAdicionais = salarioBruto
+                .add(adicionalPericulosidade)
+                .add(adicionalInsalubridade);
+
+        folha.setSalarioBruto(salarioBrutoComAdicionais); // Atualiza Salário Bruto na folha
+
+        // INSS
+        BigDecimal valorDescontoINSS = calculoINSS.calcularDesconto(salarioBrutoComAdicionais.doubleValue());
+        folha.setValorINSS(valorDescontoINSS);
+
+        // FGTS
+        BigDecimal valorFGTS = calculoFGTS.calcularFGTS(salarioBrutoComAdicionais.doubleValue());
+        folha.setValorFGTS(valorFGTS);
+
+        // IRRF (após dedução do INSS)
+        double baseIRRF = salarioBrutoComAdicionais.doubleValue() - valorDescontoINSS.doubleValue();
+        BigDecimal valorDescontoIRRF = calculoIRRF.calcularDesconto(baseIRRF);
+        folha.setValorIRRF(valorDescontoIRRF);
+
+        // Salário Líquido = Salário Bruto com Adicionais - INSS - IRRF
+        BigDecimal salarioLiquido = salarioBrutoComAdicionais
+                .subtract(valorDescontoINSS)
+                .subtract(valorDescontoIRRF)
+                .setScale(2, RoundingMode.HALF_UP);
+
+        folha.setSalarioLiquido(salarioLiquido);
 
         return folha;
     }
