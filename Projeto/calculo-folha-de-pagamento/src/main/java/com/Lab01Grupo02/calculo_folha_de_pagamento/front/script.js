7ac35a03f156b46c46761bd5a0720bd4efdb5dbc
Iagolara <iagodekara@gmail.com>
2025-12-01 14:04:58 -0300

    Add HTML files for folha de pagamento, home, and relatorio; implement script for form handling and navigation

---
+++ Projeto/calculo-folha-de-pagamento/src/main/java/com/Lab01Grupo02/calculo_folha_de_pagamento/front/script.js
@@ -0,0 +1,18 @@
+// No script do final da página
+function cancelarCadastro() {
+    if(confirm("Deseja cancelar?")) {
+        window.location.href = "funcionarios.html"; // Volta para a lista
+    }
+}
+
+document.getElementById('funcionarioForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Salvo com sucesso!");
+    window.location.href = "funcionarios.html"; // Volta para a lista
+});
+
+// Dentro da função iniciarCalculo(), no setTimeout final:
+setTimeout(() => {
+    alert("Redirecionando para os Relatórios...");
+    window.location.href = "relatorios.html"; // Vai para a tela de relatórios
+}, 1000);
